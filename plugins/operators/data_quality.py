ffa69fe7f380d8cdc7b473521100747898e948f3
ThiagoPositeli <thiago.positeli@hotmail.com>
2022-09-09 09:45:35 -0300

    parameters errors

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,21 +8,28 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id,
-                 tables,
+                 redshift_conn_id="",
+                 checks=[],
+                 ignore_fails=True,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.tables = tables
+        self.checks = checks
+        self.ignore_fails = ignore_fails
 
     def execute(self, context):
-        redshift_hook = PostgresHook(self.redshift_conn_id)
-        for table in self.tables:
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {table} returned no results")
-            num_records = records[0][0]
-            if num_records < 1:
-                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
-            self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        for check in self.checks:
+            sql = check.get("check_sql")
+            exp_result = check.get("expected_result")
+            self.log.info(
+                "Checking: " + "(" + str(sql) + ")" + " is equal " + str(exp_result))
+            records = redshift.get_records(sql)[0]
+            if records[0] == exp_result:
+                self.log.info("Check passed")
+            elif not self.ignore_fails and records[0] != exp_result:
+                ValueError("Data Quality Check failed.")
+            else:
+                self.log.info("Check failed")
+        self.log.info("Checking Data Quality Done!")
